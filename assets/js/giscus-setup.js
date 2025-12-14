242b2f6d7a946a8d0f96b3058138738ac54da9a1
guleedali <167659755+guleedali@users.noreply.github.com>
2025-12-14 18:19:53 +0000

    Deploying to gh-pages from @ guleedali/guleedali.github.io@d8f1ed0f0bb0edab2dbcf32a2ede523465591c21 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "guleedali/guleedali-guleedali.github.io",
+    "data-repo": "guleedali/guleedali.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
